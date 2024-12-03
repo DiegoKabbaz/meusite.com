788f7c62097e376778b6abdbf37a5dde2c53ca61
Gilson Francisco dos Santos Júnior <eu_juninho@outlook.com>
2024-12-03 18:46:07 -0300

    Revert "Atualizações 01/12"

--- meusite.com/templates/menu.php
+++
@@ -1,9 +0,0 @@
-<nav>
-    <ul>
-        <li><a href="?pg=conteudo">Home</a></li>
-        <li><a href="?pg=quemsomos">Quem Somos</a></li>
-        <li><a href="?pg=produtos">Produtos</a></li>
-        <li><a href="?pg=clientes">clientes</a></li>
-        <li><a href="?pg=faleconosco">Fale Conosco</a></li>
-    </ul>
-</nav>
